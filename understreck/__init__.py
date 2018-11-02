3e831782150450bf3a49ab763c90bee981ada0b2
Carl-Fredrik Arvidson <carl-fredrik@arvidson.io>
2018-11-02 15:44:45 +0100

    Bump version: 0.1.0 → 0.1.1

--- understreck/__init__.py
+++ understreck/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Carl-Fredrik Arvidson"""
 __email__ = "carl-fredrik@arvidson.io"
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.1.1'"
 
 from .nested_get import nested_get
